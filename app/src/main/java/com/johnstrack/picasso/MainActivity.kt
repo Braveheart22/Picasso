8f67d8e59af4b6b507e5752fdf111664e9debfd1
John Strack <Braveheart22@GMail.com>
2018-04-05 13:20:59 -0400

    First image in main activity

--- app/src/main/java/com/johnstrack/picasso/MainActivity.kt
+++ app/src/main/java/com/johnstrack/picasso/MainActivity.kt
@@ -2,11 +2,19 @@ package com.johnstrack.picasso
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Picasso.get()
+                .load("http://i.imgur.com/DvpvklR.png")
+                .resize(1000, 1000)
+                .centerCrop()
+                .into(imageView)
     }
 }
